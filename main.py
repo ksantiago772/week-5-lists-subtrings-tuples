c2a2ecd8ab79de0b79cefdb652d2f0e575ea6821
Kevin Santiago <113930456+ksantiago772@users.noreply.github.com>
2022-09-19 17:41:44 +0000

    added words in different positions, added items to the end of the list

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ my_list = ["bay","ddaa", 34, 56,78,89,102]
 my_list.append("jewelry")
 print(my_list)
 my_list.pop()
-#pop() removes from the list
+#pop() removes last item from the list
 print(my_list)
 my_list.remove("ddaa")
 #removes an item at a certain index
@@ -28,7 +28,10 @@ print(my_list)
 my_list.insert(3,"bobba bobba")
 print(my_list)
 
-
+my_list2 = ["july", "terrence", "jugga", "star strek"]
+#join the my_list with my_list2 in a new variable 
+#my_list3 = my_list + my_list2
+#print(my_list3)
 
 
 
@@ -53,25 +56,28 @@ luckyNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15]
 friends = ["Kevin", "Karen", "jim", "oscar", "tim", "lord tennyson"]
 # instructions:
 ######join both lists together in a new list
-
+my_list3 = luckyNumbers + friends
+print(my_list3)
 ###### print the new list out
-#print(newList2)
+print(my_list3)
 ###### print out the first item in the list
+print(my_list3[1])
 
 ###### remove the last item of the new list
-
+my_list3.pop()
 ###### print the new list out without the last item
+print(my_list3)
 
 ###### add three more items to the end of the list
-
+my_list3.append("moises,monica,evins")
 ###### print the last item in the new list
-
+print(my_list3)
 ###### sort the list
-
+#print(my_list3.sort())
 ###### find a way to insert a new item at the 3rd position of the list
-
+my_list3.insert(3,"phone")
 ###### print the new list out
-
+print(my_list3)
 ##### reverse the list
 
 ###### print the new list out
